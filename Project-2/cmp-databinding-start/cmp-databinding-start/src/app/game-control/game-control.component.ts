324d2e7509561c252cc6730e58c862a9769e23b4
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2022-11-27 23:46:47 +0100

    Assigment 4 Practicing Property and Event Binding and View Encapsulation - task 2,3 done

--- Project-2/cmp-databinding-start/cmp-databinding-start/src/app/game-control/game-control.component.ts
+++ Project-2/cmp-databinding-start/cmp-databinding-start/src/app/game-control/game-control.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
+  count: number = 0;
+  interval: any;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  gameCounter() {
+    this.count++;
   }
 
+  setGameInterval() {
+    this.interval = setInterval(() => {
+      this.count++;
+      console.log(this.count);
+    }, 1000);
+  }
 }
